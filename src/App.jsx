5df914b22a445a3760373523b690a2226069f2b6
Romulo Rosa <romulorosadev@gmail.com>
2024-05-28 21:41:24 -0300

    aula 02 app.jsx exibir nome sobrenome

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //Importada a lib React e a funcionalidade especifica/Hook useState
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 
@@ -7,32 +7,16 @@ import './App.css';
 //É renderizada pelo Componente Main
 function App() {
 
-  //contador - Valor inicial, já que informo que o stado inicial de contador tem valor 0
-  //setContador - Valor de transição. Modifica o valor de contador através do parametro passado em sua chamada
-  const [contador, setContador] = useState(0);
-
-  //incrementar é uma função anônima
-  const incrementar = () => {
-    //Ela chama a função setContador e passa o seu parametro
-    setContador(contador + 1);
-  }
-
-  //decrementar é uma função anônima
-  const decrementar = () => {
-    //Ela chama a função setContador e passa o seu parametro
-    setContador(contador - 1);
-  }
-
+  //Criadas duas variaveis pra armazenar nome e sobrenome
+  const inNome = 'Romulo'
+  const inSobrenome = 'Rosa'
 
   //É o retorno da função App
   return (
     //É o que será renderizado dentro de main e que será inserido na index.html
     <>
-      <p>Vc clicou {contador} vezes</p>
-
-      <button onClick={incrementar}>+</button>
-
-      <button onClick={decrementar}>-</button>
+      <p>NOME: {inNome}</p>
+      <p>SOBRENOME: {inSobrenome}</p>
     </>
   )
 }
